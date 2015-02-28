e01d1e46d973a7fd7e858ef8a34e85547b4b318e
scottkao85 <scottkao85@gmail.com>
2015-02-27 20:09:27 -0800

    Add schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,12 +3,26 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  objectId int(10) NOT NULL auto_increment,
+  username varchar(20),
+  text varchar(200),
+  createdAt timestamp(6),
+  roomname varchar(20),
+  PRIMARY KEY (objectId)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  username varchar(20) NOT NULL,
+  screenname varchar(20),
+  PRIMARY KEY (username)
+);
 
-
+CREATE TABLE rooms(
+  roomname varchar(20),
+  description varchar(100),
+  PRIMARY KEY (roomname)
+);
 
 
 /*  Execute this file from the command line by typing:
